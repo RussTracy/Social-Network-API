a149b04bfeb7d257a35e81cc1f72ff37530c70ca
Russ <ocjade@comcast.net>
2021-03-20 14:04:20 -0600

    User model and API routes

---
+++ controllers/user-controller.js
@@ -0,0 +1,70 @@
+const { User } = require('../models');
+
+const userController = {
+    getAllUsers(req, res) {
+        User.find({})
+            .select('-__v')
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+            .select('-__v')
+            .then(dbUserData => {
+                // If no user is found, send 404
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with that ID' })
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // Create user
+    createUser({ body }, res) {
+        User.create(body)
+            .then(dbUserData => res.json(dbUserData))
+            .catch(err => res.status(400).json(err));
+    },
+
+    // Update user
+    updateUser({ params, body }, res) {
+        User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
+            .then(dbUserData => {
+                // If no user is found, send 404
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with that ID' })
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+
+    // Delete Pizza
+    deleteUser({ params }, res) {
+        User.findOneAndDelete({ _id: params.id })
+            .then(dbUserData => {
+                if (!dbUserData) {
+                    res.status(404).json({ message: 'No user found with this id!' });
+                    return;
+                }
+                res.json(dbUserData);
+            })
+            .catch(err => res.status(400).json(err));
+    }
+
+};
+
+module.exports = userController;
